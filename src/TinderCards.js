6bd9c107d98162c63b2272d92a3a0eda0b802314
Chase <chaseburkdoll@gmail.com>
2023-06-03 11:38:34 -0500

    Used axios to connect to database

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,18 +1,20 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import "./TinderCards.css"
 import TinderCard from 'react-tinder-card'
+import axios from './axios'
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-        name: "Alex Honnold",
-        url: "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRjBM6hf-xoCwF1jXrO8tsQkAEKWXFAvZRN3JLdhS0Q1qCJl3Bm10zOzEPcckqfRa1o2ZtPMehQKtuzeBI"
-    },
-        {
-        name: "Markis Nguyen",
-        url: "https://media.licdn.com/dms/image/C4D03AQEvPj7YrjPepA/profile-displayphoto-shrink_800_800/0/1618274910580?e=1691020800&v=beta&t=a50m7Xd-D65Xk4hQbeVtpCN5J19KgTXH1mFFm7gJOyY"
-    }
-    ])
+    const [people, setPeople] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get('/tinder/cards')
+            setPeople(req.data)
+        }
+        fetchData()
+    }, [])
+
+    console.log(people)
 
     const swiped = (direction, nameToDelete) => {
         console.log("removing: " + nameToDelete)
@@ -29,11 +31,11 @@ function TinderCards() {
             <TinderCard
                 className='swipe'
                 key={person.name}
-                preventSwipe={["up", "down"]}
+                preventSwipe={['up', 'down']}
                 onSwipe={(dir) => swiped(dir, person.name)}
                 onCardLeftScreen={() => outOfFrame(person.name)}
                 >
-                <div style={ {backgroundImage: `url(${person.url})`}}
+                <div style={ {backgroundImage: `url(${person.imgUrl})`}}
                 className="card">
                     <h3>{person.name}</h3>
                 </div>
